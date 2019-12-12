94b39ced17993d021f2ba7c8da4bfe8e760896b0
Mira <myrahosd@hotmail.fr>
2019-12-12 20:51:19 +0100

    done

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,8 +1,11 @@
 const express        = require("express");
 const passportRouter = express.Router();
 // Require user model
+const User = require("../models/user");
 
 // Add bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
 // Add passport 
 
@@ -13,5 +16,29 @@ const ensureLogin = require("connect-ensure-login");
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
+
+passportRouter.get("/signup", function (req,res,next){
+  res.render('passport/signup')
+})
+
+passportRouter.post("/signup", function (req,res,next){
+  const username = req.body.username;
+  const password = req.body.password;
+
+    // 1. Check username and password are not empty
+    if (username === "" || password === "") {
+      res.render("auth/signup", { errorMessage: "Indicate username and password" });
+      return;
+    }
+  
+    User.findOne({ username })
+      .then(user => {
+        // 2. Check user does not already exist
+        if (user) {
+          res.render("auth/signup", { errorMessage: "The username already exists" });
+          return;
+       }
+    });
+})
 
 module.exports = passportRouter;
